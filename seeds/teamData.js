6abe998a1b236e4cbe7510f0f4be929c42deab3b
JD Marty <joshua.dmarty@gmail.com>
2020-12-19 11:56:08 -0800

    added basic seed data

---
+++ seeds/teamData.js
@@ -0,0 +1,20 @@
+const { Team } = require('../models');
+
+const teamData = [
+  {
+    name: "Lions",
+    user_id: 1
+  },
+  {
+    name: "Tigers",
+    user_id: 2
+  },
+  {
+    name: "Bears",
+    user_id: 3
+  },
+];
+
+const seedTeams = () => Team.bulkCreate(teamData)
+
+module.exports = seedTeams;
