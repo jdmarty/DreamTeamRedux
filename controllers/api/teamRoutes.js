0bcfdf24bd4e048ccdbd5a3486793be12b06f557
JD Marty <joshua.dmarty@gmail.com>
2020-12-19 13:01:09 -0800

    added put and delete routes to api/teams

--- controllers/api/teamRoutes.js
+++ controllers/api/teamRoutes.js
@@ -54,4 +54,46 @@ router.post("/", async (req, res) => {
   }
 });
 
+//update a team name
+router.put("/:id", async (req, res) => {
+    try {
+      const updatedTeam = await Team.update(req.body, {
+        where: { id: req.params.id },
+      });
+
+      if (!updatedTeam[0]) {
+        res.status(404).json({ message: "No team update performed" });
+        return;
+      }
+
+      res.status(200).json({
+        teamsUpdated: updatedTeam[0],
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+})
+
+//delete a team
+router.delete("/:id", async (req, res) => {
+  try {
+    //find all TeamPlayers for this project
+    const teamPlayersToRemove = await TeamPlayer.findAll({
+      where: { team_id: req.params.id },
+    });
+    const teamPlayerIdsToRemove = teamPlayersToRemove.map(({ id }) => id);
+    //remove Teams and TeamPlayers that match the provided ids
+    const removedItems = await Promise.all([
+      TeamPlayer.destroy({ where: { id: teamPlayerIdsToRemove } }),
+      Team.destroy({ where: { id: req.params.id } }),
+    ]);
+    res.status(200).json({
+      deletedTeams: removedItems[1],
+      deletedTeamPlayers: removedItems[0],
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
