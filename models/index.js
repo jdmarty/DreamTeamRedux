0f98b19b5860f265ecc7bb4b119e2837926c5b1f
JD Marty <joshua.dmarty@gmail.com>
2020-12-19 10:00:46 -0800

    updates to association of models

--- models/index.js
+++ models/index.js
@@ -1,31 +1,31 @@
-const User = require('./User');
-const Team = require('./Team');
-const TeamPlayer = require('./TeamPlayer');
-const Player = require('./Player');
+const User = require("./User");
+const Team = require("./Team");
+const TeamPlayer = require("./TeamPlayer");
+const Player = require("./Player");
 
 User.hasMany(Team, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 Team.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
 Player.belongsToMany(Team, {
   through: {
     model: TeamPlayer,
-    unique: false
+    unique: false,
   },
-  as: 'teams'
+  as: "teams",
 });
 
 Team.belongsToMany(Player, {
   through: {
     model: TeamPlayer,
-    unique: false
+    unique: false,
   },
-  as: 'players'
-})
+  as: "players",
+});
 
 module.exports = { User, Team, Player, TeamPlayer };
