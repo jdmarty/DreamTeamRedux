f01094dbc68e84e5a10485ca0c743069ef94cebb
JD Marty <joshua.dmarty@gmail.com>
2020-12-19 10:54:33 -0800

    added login page routes and HTML

--- models/Player.js
+++ models/Player.js
@@ -16,15 +16,40 @@ Player.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    season: {
-      type: DataTypes.INTEGER
-    },
     offence: {
       type: DataTypes.INTEGER,
       allowNull: false,
     },
     defence: {
       type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    twoPtPercent: {
+      type: DataTypes.DECIMAL(10, 3),
+      allowNull: false,
+    },
+    threePtPercent: {
+      type: DataTypes.DECIMAL(10, 3),
+      allowNull: false,
+    },
+    ppg: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    oreb: {
+      type: DataTypes.DECIMAL(10, 3),
+      allowNull: false,
+    },
+    dreb: {
+      type: DataTypes.DECIMAL(10, 3),
+      allowNull: false,
+    },
+    steals: {
+      type: DataTypes.DECIMAL(10, 3),
+      allowNull: false,
+    },
+    blocks: {
+      type: DataTypes.DECIMAL(10, 3),
       allowNull: false,
     },
   },
