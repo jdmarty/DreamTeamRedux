c7f0b6c0eca8aba8ec91e44edd7f1dad24fd5837
JD Marty <joshua.dmarty@gmail.com>
2020-12-21 17:30:47 -0800

    removed old route from miniproject

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -36,26 +36,6 @@ router.get('/profile', async (req, res) => {
     }
 })
 
-router.get('/project/:id', async (req, res) => {
-  try {
-    const projectData = await Project.findOne({
-      where: { id: req.params.id },
-      attributes: { exclude: ['user_id'] }
-    });
-    console.log(projectData);
-    if (!projectData) {
-      res.status(404).json({ message: "No project with this id"})
-    }
-    const project = projectData.get({ plain: true });
-    res.render('project', {
-      project,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-})
-
 router.get('/login', (req, res) => {
   // If a session exists, redirect the request to the homepage
   if (req.session.logged_in) {
