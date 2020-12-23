ed1def8e125fa50efd61cee335abd4656ccfea90
JD Marty <joshua.dmarty@gmail.com>
2020-12-22 21:05:58 -0800

    started work on homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,44 +1,46 @@
-const router = require('express').Router();
-const { User } = require('../models');
+const router = require("express").Router();
+const { User, Team, Player, TeamPlayer } = require("../models");
+const axios = require("axios");
 
 // Prevent non logged in users from viewing the homepage
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    console.log('here')
-    res.render("login", {
-      logged_in: req.session.logged_in
-    })
+
+  } catch (err) {
+    console.log(err)
+    res.status(505).json(err);
+  }
+});
+
+
+
+
+router.get("/runGame", async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { id: req.session.user_id },
+    });
+    if (!userData) {
+      res.status(404).json({ message: "No user with this id" });
+    }
+    const user = userData.get({ plain: true });
+    res.render("profile", {
+      user,
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/runGame', async (req, res) => {
-    try {
-      const userData = await User.findOne({
-        where: { id: req.session.user_id },
-      });
-      if (!userData) {
-        res.status(404).json({ message: 'No user with this id' });
-      }
-      const user = userData.get({ plain: true });
-      res.render('profile', {
-        user,
-        logged_in: req.session.logged_in,
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-})
-
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // If a session exists, redirect the request to the homepage
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = router;
