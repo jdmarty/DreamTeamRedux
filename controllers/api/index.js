37f3965b622ed6e92a252c7a3498803be6de07f1
JD Marty <joshua.dmarty@gmail.com>
2020-12-29 18:38:39 -0800

    changed team routes to create teamPlayer tags in request

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,9 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const teamRoutes = require('./teamRoutes');
 const playerRoutes = require('./playerRoutes');
-const teamPlayerRoutes = require('./teamPlayerRoutes');
 
 router.use('/users', userRoutes);
 router.use('/teams', teamRoutes);
 router.use('/players', playerRoutes);
-router.use('/teamPlayer', teamPlayerRoutes);
 
 module.exports = router;
