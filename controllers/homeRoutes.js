3a80115084c3c24ab92e99a3bc32971acc5ee7c2
JD Marty <joshua.dmarty@gmail.com>
2021-01-05 19:22:54 -0800

    added homepage and fixed user routes for login

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,75 +5,26 @@ const withAuth = require('../util/auth')
 //If authorization passes, render homepage
 router.get("/", withAuth, async (req, res) => {
   try {
-    //find all teams for this user
+    //get all teams for this user
     const userTeamsData = await Team.findAll({
-      where: { user_id: req.session.user_id }
-    })
+      where: { user_id: req.session.user_id || 2 },
+    });
     //serialize teams
-    const userTeams = userTeamsData.map(team => team.get({ plain: true }))
-    //send array of teams to handlebars
-    res.render("login", {
+    const userTeams = userTeamsData.map((team) => team.get({ plain: true }));
+
+    res.render("homepage", {
       logged_in: req.session.logged_in,
       userTeams
     });
   } catch (err) {
+    console.log(err)
     res.status(500).json(err);
   }
 });
 
-//Route to edit an existing team
-router.get("/team/:id", withAuth, async (req, res) => {
-  try {
-    //get data for an existing team
-    const teamData = await Team.findByPk(req.params.id, {
-      include: [
-        { model: User, attributes: ["id", "name"] },
-        { model: Player, through: TeamPlayer, as: "players" },
-      ],
-    });
-    //serialize the data
-    const team = teamData.get({ plain: true })
-    res.render("login", {
-      logged_in: req.session.logged_in,
-      team,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-})
 
-//Route to create a new team
-router.get("/new-team", withAuth, async (req, res) => {
-  try {
-    res.render("login", {
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-//Route to go to the run game page
-router.get("/game", withAuth, async (req, res) => {
-  try {
-    //find all teams for this user
-    const userTeamsData = await Team.findAll({
-      where: { user_id: req.session.user_id }
-    })
-    //serialize teams
-    const userTeams = userTeamsData.map(team => team.get({ plain: true }))
-    //send array of teams to handlebars
-    res.render("login", {
-      logged_in: req.session.logged_in,
-      userTeams
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-//Route to visit testsim page
-router.get("/testsim", async (req, res) => {
+//Route to visit game page
+router.get("/game", async (req, res) => {
   try {
     let homeTeam, awayTeam
     //find all teams for this user
@@ -101,9 +52,9 @@ router.get("/testsim", async (req, res) => {
 
     //create a variable to describe if the game is ready
     const ready = homeTeam && awayTeam ? true : false
-
+    console.log(userTeams);
     //render the page with retrieved data
-    res.render("testsim", {
+    res.render("game", {
       logged_in: req.session.logged_in,
       userTeams,
       homeTeam,
@@ -113,7 +64,6 @@ router.get("/testsim", async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-
 })
 
 //Route to visit the login page
