e63f137ec7eae5eda77468d00121cd407d5f4cf3
JD Marty <joshua.dmarty@gmail.com>
2021-01-07 21:29:09 -0800

    added npm seed route for post-build

--- seeds/seed.js
+++ seeds/seed.js
@@ -9,17 +9,17 @@ const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log("\n----- DATABASE SYNCED -----\n");
 
-  await seedUsers();
-  console.log("\n----- USERS SEEDED -----\n");
+  // await seedUsers();
+  // console.log("\n----- USERS SEEDED -----\n");
 
-  await seedTeams();
-  console.log("\n----- TEAMS SEEDED -----\n");
+  // await seedTeams();
+  // console.log("\n----- TEAMS SEEDED -----\n");
 
   await seedPlayers();
   console.log("\n----- PLAYERS SEEDED -----\n");
 
-  await seedTeamPlayer();
-  console.log("\n----- TEAM PLAYERS SEEDED -----\n");
+  // await seedTeamPlayer();
+  // console.log("\n----- TEAM PLAYERS SEEDED -----\n");
 
   process.exit(0);
 };
